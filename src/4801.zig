6e0fa6136220293e674fe8d37c6a1d5d9476954c
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2025-07-04 20:01:52 -0400

    fix clock to be slower, matching original CPU

--- src/4801.zig
+++ src/4801.zig
@@ -8,14 +8,13 @@ pub var setTime: i128 = undefined;
 pub fn tick() void {
     const now: i128 = std.time.nanoTimestamp();
 
-    if (now - setTime >= 400) {
-        if (lastP == 1) {
-            p1 = true;
-            lastP = 0;
-        } else {
-            p2 = true;
-            lastP = 1;
-        }
+    if (now - setTime >= 650 and lastP == 1) {
+        p1 = true;
+        lastP = 0;
+        setTime = now;
+    } else if (now - setTime >= 900 and lastP == 0) {
+        p2 = true;
+        lastP = 1;
         setTime = now;
     }
 }
